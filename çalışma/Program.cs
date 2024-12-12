8b5ea72e2907ca54825cf9ab0ab7b72717fb6997
Nedim-furkannfk2010 <nedimkucukabaz2010@gmail.com>
2024-12-12 23:46:15 +0300

    Add project files.

---
+++ çalışma/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace çalışma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Alt alta 10 tane yıldız oluşturma
+            //for (int i=0; i<=10;i++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+
+
+
+
+
+
+            #endregion
+
+            #region Yan yana 10 tane yıldız oluşturma
+
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    Console.Write("*");
+            //}
+
+
+
+
+
+
+            #endregion
+
+            #region  Yan yana 10 tane yıldız oluşturma ve her satırda 10 tane yıldız oluşturma
+
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    Console.WriteLine("**********");
+            //}
+
+
+
+
+
+
+
+            #endregion
+
+            #region Dik üçgen
+            //for(int i=1;i<=5;i++)
+            //{
+            //    for(int j=1;j<=i;j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    {
+            //        Console.WriteLine();
+
+            //    }
+
+
+            //}
+
+
+
+            #endregion
+
+            #region Ters Dik üçgen
+            //for (int i = 5; i >= 1; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    {
+            //        Console.WriteLine();
+
+            //    }
+
+
+            //}
+            #endregion
+
+            #region Dik üçgen ve ters üçgen beraber
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    {
+            //        Console.WriteLine();
+
+            //    }
+
+
+            //}
+
+
+
+            //for (int i = 4; i >= 1; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    {
+            //        Console.WriteLine();
+
+            //    }
+
+
+            //}
+
+
+
+            #endregion
+
+
+
+
+
+
+
+
+
+            Console.Read();
+        }
+    }
+
+
+
+
+
+}
